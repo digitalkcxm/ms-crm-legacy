c562cbccf5b9bd7d41e2f584c96e9e034d504caf
Silas Silva <silas.silva@rockt.com.br>
2019-08-07 14:24:15 -0300

    Corrige erro de async/await

--- src/services/customer-service.js
+++ src/services/customer-service.js
@@ -23,23 +23,23 @@ async function schedulePersist(dataCustomers, companyToken) {
 async function persistCustomer(dataCustomer) {
   try {
     const customerId = await newCustomer.createOrUpdate(dataCustomer.customer.company_token, dataCustomer.customer.cpfcnpj, dataCustomer.customer)
-    dataCustomer.email.forEach(email => {
+    await dataCustomer.email.forEach(async (email) => {
       await newEmail.create(customerId, email.email)
     })
     
-    dataCustomer.phone.forEach(phone => {
+    await dataCustomer.phone.forEach(async (phone) => {
       await newPhone.createOrUpdate(customerId, phone)
     })
     
-    dataCustomer.address.forEach(address => {
+    await dataCustomer.address.forEach(async (address) => {
       await newAddress.createOrUpdate(customerId, address)
     })
 
-    dataCustomer.vehicle.forEach(vehicle => {
+    await dataCustomer.vehicle.forEach(async (vehicle) => {
       await newVehicle.createOrUpdate(customerId, vehicle)
     })
 
-    dataCustomer.business_partner.forEach(businessPartner => {
+    await dataCustomer.business_partner.forEach(async (businessPartner) => {
       await newBusinessPartner.createOrUpdate(customerId, businessPartner)
     })
   } catch (err) {
